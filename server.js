fa8025c270ce05b0ea493d08cacf27013d9455ba
Shivam Kapoor <mailtoshivamk@gmail.com>
2017-08-27 17:01:52 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var morgan = require('morgan');
 var path = require('path');
 var Pool = require('pg').Pool;
 var crypto = require('crypto');
+var bodyParser = require('body-parser');
 
 var config = {
     user: 'mailtoshivamk',
@@ -15,6 +16,7 @@ var pool = new Pool(config);
 
 var app = express();
 app.use(morgan('combined'));
+app.use(bodyParser.json());
 
 function createArticleTemplate(data) {
   var title = data.title;
@@ -56,6 +58,21 @@ app.get('/', function (req, res) {
 app.get('/hash/:input', function(req, res) {
     var hashedString = hash(req.params.input, 'this-is-some-random-string');
     res.send(hashedString);
+});
+
+app.post('/create-user', function(req, res) {
+    var username = req.body.username;
+    var password = req.body.password;
+    
+    var salt = cryto.randomBytes(128).toString('hex');
+    var dbString = hash(password, salt);
+    pool.query('INSERT INTO "user" (username, password) VALUES ($1, $2)', [username, dbString], function(err, result) {
+        if(err) {
+            res.status(500).send(err.toString());
+        } else {
+            res.send('User successfully created: ' + username);
+        }
+    });
 });
 
 var names = [];
