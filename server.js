580b66ff8aa9c7983eb55037a2c8d7bffaf0773b
Shivam Kapoor <mailtoshivamk@gmail.com>
2017-08-27 17:20:59 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -75,6 +75,30 @@ app.post('/create-user', function(req, res) {
     });
 });
 
+app.post('/login', function(req, res) {
+    var username = req.body.username;
+    var password = req.body.password;
+    
+    pool.query('SELECT * FROM "user" WHERE username = $1', [username], function (err, result) {
+        if(err) {
+            res.status(500).send(err.toString());
+        } else {
+            if(result.rows.length === 0) {
+                res.send(403).send('Username/Password is invalid.');
+            } else {
+                var dbString = result.rows[0].password;
+                var salt = dbString.split('$')[2];
+                var hashedPassword = hash(password, salt);
+                if(hashedPassword === dbString) {
+                    res.send('credentials correct!');
+                } else {
+                    res.send(403).send('Username/Password is invalid.');
+                }
+            }
+        }
+    });
+});
+
 var names = [];
 app.get('/addName/:name', function (req, res) {
   var name = req.params.name;
