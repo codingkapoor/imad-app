3c581eeee5d7ca3d79ed35d21c5f2369b0e1c492
Shivam Kapoor <mailtoshivamk@gmail.com>
2017-08-27 17:57:14 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -114,7 +114,10 @@ app.get('/check-login', function(req, res) {
     }
 });
 
-
+app.get('/logout', function(req, res) {
+    delete req.session.auth;
+    res.send('Logged out!');
+});
 
 app.get('/articles/:articleName', function (req, res) {
   var articleName = req.params.articleName;
