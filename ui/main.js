284e2bae6e10c5dfa7dd7bd205e7e3f4efeb42c9
Shivam Kapoor <mailtoshivamk@gmail.com>
2017-08-27 17:38:30 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,11 +4,17 @@ console.log('Loaded!');
 var submit = document.getElementById('submit_btn');
 submit.onclick = function () {
 
+    var username = document.getElementById('username').value;
+    var password = document.getElementById('password').value;
+    
     // Create a request
     var request = new XMLHttpRequest();
-
-    request.onreadystatechange = function () {
-    	if(request.readyState === XMLHttpRequest.DONE) {
+   request.open('POST', 'http://mailtoshivamk.imad.hasura-app.io:80/login' + name, true);
+   request.setRequestHeader('Content-Type', 'application/json');
+   request.send(JSON.stringify({username: username, password: password})); 
+   
+   request.onreadystatechange = function () {
+        if(request.readyState === XMLHttpRequest.DONE) {
     	    if(request.status === 200) {
     	        alert('Logged in successfully!');
     	    } else if(request.status === 403) {
@@ -18,12 +24,4 @@ submit.onclick = function () {
     	    }
     	}
     }
-
-    // Make a request 
-    var username = document.getElementById('username').value;
-    var password = document.getElementById('password').value;
-    
-   request.open('POST', 'http://mailtoshivamk.imad.hasura-app.io:80/login' + name, true);
-   request.setRequestHeader('Content-Type', 'application/json');
-   request.send(JSON.stringify({username: username, password: password})); 
 };
